e2ea67e026f23a9f495cf9a98fa0f9bb5649f130
CloudsWeight <CloudsWeight@users.noreply.github.com>
2024-10-01 02:19:52 -0700

    Update bea.py

--- bea.py
+++ bea.py
@@ -1,112 +1,135 @@
-'''
-BEA testing
-
-'''
 #!/usr/bin/env python
 
 import requests
 from key import BEA_Key
 import json
-class BEAUrl:
+
+class BeaData():
+    '''
+    Basic usage is to create a bd object and make requests to get datasets and parameters.
+    The BEA API method is to find your dataset, inspect the list of parameters, 
+    find the parameter you like then get the parameter values, then use that to build
+    a request to get data from the specific table values you need.
+    Here is an example URL to retrieve JSON data from a dataset for all years: 
+
+    'https://apps.bea.gov/api/data?&UserID=\
+{self.bea_key}&method=GetData&datasetname=FixedAssets&TableName=FAAt105&year=x&ResultFormat=JSON'
+
+    Basic Data Retrieval syntax examples for BEA API:
+        - method = (GetParameterList, GetData, GetParameterValues)
+        - dataset = 'FixedAssets'
+        - ParameterName = TableName,
+        - TableName = FAAt107
+        - year = available or 'x' for all
+    
+    Its up to you to find the specific datasets needed.
+
+    DatasetNames: ( see req_sets())
+    NIPA
+    NIUnderlyingDetail
+    MNE
+    FixedAssets
+    ITA
+    IIP
+    InputOutput
+    IntlServTrade
+    IntlServSTA
+    GDPbyIndustry
+    Regional
+    UnderlyingGDPbyIndustry
+    APIDatasetMetaData
+
+    '''
+
     def __init__(self):
-        self.bea_key = self.set_key()
+        self.bea_key = self._bea_key()
         self.format = 'JSON'
-        self.params = self.set_bea_params_url()
-        self.format = self.set_format()
+        self.base = f'https://apps.bea.gov/api/data?&UserID={self.bea_key}&method='
 
     def __str__(self):
-        return self.params
+        return str(self.get_data())
 
     def _bea_key(self):
         bea_key = BEA_Key()
         return bea_key.key
+    # urls
+    def url_sets(self): # URL only request all the datasets
+        bea_url = f'https://apps.bea.gov/api/data?&UserID=\
+{self.bea_key}&method=GETDATASETLIST&ResultFormat=JSON'
+        return bea_url 
 
-    def set_key(self):
-        '''set API key'''
-        return self._bea_key()
+    def url_param_list(self, dataset="ITA" ):
+        method='GetParameterList'
+        bea_url = f'https://apps.bea.gov/api/data?&UserID=\
+{self.bea_key}&method={method}&datasetname={dataset}&ResultFormat=JSON'
+        return bea_url 
 
-    def set_bea_params_url(self):
-         bea_url = f'https://apps.bea.gov/api/data?&UserID={self.bea_key}&method=GETDATASETLIST&{self.format}'
-         return bea_url
-         '''build up an url for a request'''
+    def url_param_vals(self, dataset="ITA" ):
+        method='GetParameterValues'
+        bea_url = f'https://apps.bea.gov/api/data?&UserID=\
+{self.bea_key}&method={method}&datasetname={dataset}&ResultFormat=JSON'
+        return bea_url 
 
-    def set_format(self, format = 'JSON'):
-        formated = f'&ResultFormat={format}'
-        return str(formated)
+# data request
+    def req_dt(self, dataset='FixedAssets', table="FAAt105"):
+        method = 'GetData'
+        bea_url = f'https://apps.bea.gov/api/data?&UserID=\
+{self.bea_key}&method={method}\
+&datasetname={dataset}\
+&TableName={table}&year=x&ResultFormat=JSON'
+# split url for easier typing
+        return bea_url #{self.bea_key}&method=GetData&datasetname=FixedAssets&TableName=FAAt105&year=x&ResultFormat=JSON'
 
-    def test_get(self):
-        url1 = f'https://apps.bea.gov/api/data?&UserID={self.bea_key}&method=GetParameterValues&datasetname=INTLSERVTRADE&ParameterName=TradeDirection&{self.format}'
-        url2 = f'https://apps.bea.gov/api/data/?&UserID={self.bea_key}&method=GetData&datasetname=Regional&TableName=CAINC1&LineCode=3&GeoFIPS=CA&Year=2018&{self.format}'
-        r = requests.get(url2)
-        data = r.json()
-        with open('test_data.txt', 'a') as file:
-            file.write(json.dumps(data, indent=4))
-        return json.dumps(data, indent=4)
+# !raw request
+    def req(self,url = None): # basic request to any url given
+        try:
+            if url != None:
+                #print(f'\nRequesting URL: {url}\n')
+                r = requests.get(url)
+                data = json.dumps(r.json(), indent=1)
+                return data
+            else:
+                print('You need to pass an URL')
+                pass
+        except Exception as e:
+            print(f'Encountered an error: {e}')
 
-    def get_bea_datasets(self):
-        '''return the bea datasets available'''
-        r = requests.get(self.url)
-        data = r.json()
+# loop dataset names
+    def req_sets(self):
+        # returns a list of the dataset names
+        url = self.url_sets()
+        resp = json.loads(self.req(url))
+        #print("Creating a list of the dataset names:")
+        val = resp["BEAAPI"]["Results"]["Dataset"]
+        n = len(val)
+        i = 0
+        da = []
+        while i < n:
+            da.append(val[i]["DatasetName"])
+            #print(val[i]["DatasetName"])
+            i+=1
+        return da
 
+    def find_all_params_all_sets(self):
+        sets = self.req_sets()
+        n, i = len(sets), 0
+        all_arr = []
+        while i < n:
+            resp = json.loads(self.req(self.url_param_list(sets[i])))
+            all_arr.append(resp)
+            i +=1
+        #print(json.dumps(all_arr, indent=1))
+        return all_arr
+
+
+
+# __ main
 def main():
-    bea1 = BEAUrl()
-    print(bea1,'\n')
-    data = bea1.test_get()
-    print(data)
+    bd = BeaData()
+    test = bd.req_dt()
+    #print(test)
+    bea_set_params = bd.find_all_params_all_sets()
+   
 if __name__ == "__main__":
     main()
 
-'''                 DOCS AND NOTES
-
-******** Response for 'url' of currently offered datasets: ******
-        'GETDATASETLIST'}]}, 'Results':
-        {'Dataset': [
-        {'DatasetName': 'NIPA', 'DatasetDescription': 'Standard NIPA tables'}, {'DatasetName': 'NIUnderlyingDetail', 'DatasetDescription': 'Standard NI underlying detail tables'},
-        {'DatasetName': 'MNE', 'DatasetDescription': 'Multinational Enterprises'},
-        {'DatasetName': 'FixedAssets', 'DatasetDescription': 'Standard Fixed Assets tables'},
-        {'DatasetName': 'ITA', 'DatasetDescription': 'International Transactions Accounts'},
-        {'DatasetName': 'IIP', 'DatasetDescription': 'International Investment Position'},
-        {'DatasetName': 'InputOutput', 'DatasetDescription': 'Input-Output Data'},
-        {'DatasetName': 'IntlServTrade', 'DatasetDescription': 'International Services Trade'},
-        {'DatasetName': 'GDPbyIndustry', 'DatasetDescription': 'GDP by Industry'},
-        {'DatasetName': 'Regional', 'DatasetDescription': 'Regional data sets'},
-        {'DatasetName': 'UnderlyingGDPbyIndustry', 'DatasetDescription': 'Underlying GDP by Industry'},
-        {'DatasetName': 'APIDatasetMetaData', 'DatasetDescription': 'Metadata about other API datasets'}]}}}
-
-
-*********************  Request - DOCS  *************************
-
-        Response object = r
-
-        We can get all the information we need from this object, r.
-
-        Requests’ simple API: all forms of HTTP request are obvious.
-            ex.) make an HTTP POST request:
-
-            r = requests.post('https://httpbin.org/post', data={'key': 'value'})
-
-         Other HTTP request types: PUT, DELETE,
-             HEAD and OPTIONS? These are all just as simple:
-
-            r = requests.put('https://httpbin.org/put', data={'key': 'value'})
-
-            r = requests.delete('https://httpbin.org/delete')
-
-            r = requests.head('https://httpbin.org/get')
-
-            r = requests.options('https://httpbin.org/get')
-
-            builtin JSON decoder, in case you’re dealing with JSON data:
-
-            import requests
-
-            r = requests.get('https://api.github.com/events')
-
-            r.json()
-
-        Use a pattern like this to save what is being streamed to a file:
-
-            with open(filename, 'wb') as fd:
-                for chunk in r.iter_content(chunk_size=128):
-                    fd.write(chunk)
-'''
