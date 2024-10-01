c0fe6c1e504f4d824f616628c2eacce53c4a674d
CloudsWeight <CloudsWeight@users.noreply.github.com>
2024-10-01 02:21:52 -0700

    Update key.py

--- src/key.py
+++ src/key.py
@@ -1,1 +1,8 @@
+class BEA_Key:
 
+    def __init__(self):
+        self.key = 'your-own-shiny-key'
+        # hint: https://apps.bea.gov/API/signup/index.cfm
+
+    def __str__(self):
+        return self.key 
